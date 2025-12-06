1dba718658ac3bbd5b204577e38505a4481c6f34
Alejandro <alejandrorcld@gmail.com>
2025-12-06 19:52:13 -0300

    commit

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const apiRoutes = require('./routes/api.js');
 
 const app = express();
+
+// Middleware
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/public', express.static('public'));
 
+// Página principal
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Ruta que FCC usa para leer los tests
+app.get('/_api/get-tests', (req, res) => {
+  res.sendFile(path.join(__dirname, 'tests', 'tests.js'));
+});
+
+// Rutas de la API
 apiRoutes(app);
 
+// Puerto
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
