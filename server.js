d7433eaa516a2b96d6963b64ccbf9515aa7d5c27
Alejandro <alejandrorcld@gmail.com>
2025-12-06 20:49:00 -0300

    commit

--- server.js
+++ server.js
@@ -5,21 +5,17 @@ const apiRoutes = require('./routes/api.js');
 
 const app = express();
 
-// Middleware
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/public', express.static('public'));
 
-// Página principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Rutas de la API
 apiRoutes(app);
 
-// Puerto
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
--- test/tests.js
+++
@@ -1,3 +0,0 @@
-// tests/tests.js
-require('./1_unit-tests.js');
-require('./2_functional-tests.js');
