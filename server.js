86c27b95a7fff4d34673f53a679d379e216ba681
Alejandro <alejandrorcld@gmail.com>
2025-12-06 21:05:18 -0300

    commit

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/public', express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Ruta que FCC usa para leer los tests
 app.get('/_api/get-tests', (req, res) => {
   res.sendFile(path.join(__dirname, 'test', 'tests.js'));
 });
