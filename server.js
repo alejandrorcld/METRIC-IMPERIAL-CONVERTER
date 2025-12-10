538d75f8ae15db0967002922f2ddf8a300d106de
Alejandro <alejandrorcld@gmail.com>
2025-12-09 22:58:45 -0300

    commit

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const app = express();
 
 dotenv.config();
 
-// habilitar CORS para todos los orígenes, incluyendo 'null'
-app.use(cors({ origin: '*' }));
+// habilitar CORS para todos los orígenes, incluso 'null'
+app.use(cors({
+  origin: (origin, callback) => {
+    // aceptar siempre, incluso si origin es null
+    callback(null, true);
+  }
+}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
